313c5112089cbed51091063f4a0235bdd468a5ac
noureh10 <noureh.it@outlook.com>
2024-08-09 21:33:17 +0200

    3 : Misc

--- src/commands/commands.py
+++ src/commands/commands.py
@@ -1,6 +1,8 @@
-from collections import Counter
 import sys
 import os
+
+wrong_num_args = "Wrong number of arguments !\n"
+line_not_valid = "Arguments are not valid !\n"
 
 class Comand:
 	arguments = None
@@ -13,9 +15,12 @@ class Comand:
 		line_len = splitted_line.__len__()
 		help_c = '../help_prompts/general_help.txt'
 		if (line_len == 2 or line_len == 3):
-			self.__is_line_valid(line, line_len)
+			if (self.__is_line_valid(splitted_line, line_len)):
+				print_err("success")
+			else:
+				self.print_err(wrong_num_args)
 		else:
-			self.print_err("Wrong number of arguments !\n")
+			self.print_err(wrong_num_args)
 			self.command_help(help_c)
 	
 	def print_err(self, message):
@@ -26,9 +31,12 @@ class Comand:
 			help_text = file.read()
 		print(help_text)
 	
+	def __command_dispatcher(self, line):
+		print(line)
+
 	def __flags_checker(self, flags):
 		sflags_array = ["v","0"]
-		if flags.startwith("-") and len(flags) > 1:
+		if flags.startswith("-") and len(flags) > 1:
 			flags = flags[1:]
 		else:
 			return False
@@ -39,7 +47,7 @@ class Comand:
 				return False
 		return True
 	
-	def __commands_checker(self, commands):
+	def __commands_checker(self, command):
 		commands_array = [
 			"interactive",
 			"status",
@@ -48,22 +56,22 @@ class Comand:
 			"version",
 			"update"
 		]
-		for command in commands:
-			if command not in commands_array:
-				return False
-		return True
+
+		for x in commands_array:
+			if x == command:
+				return True
+		return False
 	
 	def __file_checker(self, file_path):
 		return os.path.exists(file_path)
 	
-	def command_dispatcher(self, line):
-		print(line)
-	
 	def __is_line_valid(self, line, line_len):
 		if (line_len == 2):
 			if (not(self.__commands_checker(line[1]))):
+				self.print_err(line_not_valid)
 				return False
 		elif (line_len == 3):
 			if(not(self.__flags_checker(line[1]) or self.__commands_checker(line[2]))):
+				self.print_err(line_not_valid)
 				return False
 		return True
