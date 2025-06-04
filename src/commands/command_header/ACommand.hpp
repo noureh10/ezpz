a959b4ff11bd56c28ac8056b13ad7ae8c6f9d79b
noureh10 <noureh.it@outlook.com>
2025-06-04 23:48:20 +0200

    30 - Essentials added to ACommand

--- src/commands/command_header/ACommand.hpp
+++ src/commands/command_header/ACommand.hpp
@@ -2,20 +2,26 @@
  * @ Author: Nour Echaara
  * @ Create Time: 2025-05-29 19:34:45
  * @ Modified by: Nour Echaara
- * @ Modified time: 2025-06-02 22:37:15
+ * @ Modified time: 2025-06-04 23:39:00
  */
 
 #ifndef ACOMMANDS_HPP
 # define ACOMMANDS_HPP
 
 #include <iostream>
+#include <map>
+#include <set>
 
 class ACommand {
     protected:
         std::string command_arguments;
     public:
         ACommand(void);
-        ~ACommand();
+        ACommand(const std::string& cmd_args);
+        virtual ~ACommand();
+        virtual void execute() = 0;
+        static ACommand* dispatch(const std::string& cmd, const std::string& subcmd);
+        static const std::map<std::string, std::set<std::string>> getCommandMap(void);
 };
 
 # endif
