1ce2c52615939de7ee2b03681984e026d8239e49
noureh10 <noureh.it@outlook.com>
2025-06-22 23:07:12 +0200

    43 - Basic TUI structure

---
+++ src/terminal_ui/Interactive.cpp
@@ -0,0 +1,22 @@
+/**
+ * @ Author: Nour Echaara
+ * @ Create Time: 2025-06-02 23:08:37
+ * @ Modified by: Nour Echaara
+ * @ Modified time: 2025-06-22 10:49:10
+ */
+
+#include "terminal_ui_header/Interactive.hpp"
+
+Interactive::Interactive(void) {
+
+}
+
+Interactive::~Interactive(void) {
+    
+}
+
+void Interactive::execute(void) {
+    std::cout << "Placeholder for Interactive command" << std::endl;
+
+    
+}
---
+++ src/terminal_ui/terminal_ui_header/Interactive.hpp
@@ -0,0 +1,21 @@
+/**
+ * @ Author: Nour Echaara
+ * @ Create Time: 2025-06-02 23:06:05
+ * @ Modified by: Nour Echaara
+ * @ Modified time: 2025-06-15 18:29:46
+ */
+
+#ifndef INTERACTIVE_HPP
+# define INTERACTIVE_HPP
+
+#include <iostream>
+
+
+class Interactive {
+    public:
+        Interactive(void);
+        ~Interactive();
+        static void execute(void);
+};
+
+#endif
