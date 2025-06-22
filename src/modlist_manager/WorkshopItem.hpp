ee9f65a7ed7f041bd3caa5eb8b27937310b5ad8f
noureh10 <noureh.it@outlook.com>
2025-06-22 23:05:32 +0200

    40 - Updating attributes and adding getters that will be used for the Modlist class

--- src/modlist_manager/WorkshopItem.hpp
+++ src/modlist_manager/WorkshopItem.hpp
@@ -2,7 +2,7 @@
  * @ Author: Nour Echaara
  * @ Create Time: 2025-06-15 18:37:41
  * @ Modified by: Nour Echaara
- * @ Modified time: 2025-06-21 23:35:36
+ * @ Modified time: 2025-06-22 13:39:53
  */
 
 #ifndef WORKSHOPITEM_HPP
@@ -10,15 +10,25 @@
 
 #include <set>
 #include <vector>
+#include <iostream>
+#include <filesystem>
 
 class WorkshopItem {
 private:
-    unsigned int _workshopID;
+    unsigned int    _workshopID;
+    std::string&    _workshop_name;
+    std::string&    _workshop_item_descritpion;
+    std::string&    _workshop_item_path;
+    unsigned int    _workshop_item_size;
     std::set<WorkshopItem*> _dependencies;
     bool hasCycleUtil(std::set<WorkshopItem*>& visited, std::set<WorkshopItem*>& recursionStack);
 public:
     explicit WorkshopItem(unsigned int workshopID);
-    unsigned int getID() const;
+    unsigned int getID(void) const;
+    std::string& getWorkshopName(void) const;
+    std::string& getWorkshopModDescription(void) const;
+    std::string& getFormattedFilesize(void) const;
+    uintmax_t getNonFormattedFilesize(void) const;
     bool addDependency(WorkshopItem* dependency);
     const std::set<WorkshopItem*>& getDependencies() const;
     std::set<WorkshopItem*> getAllDependencies() const;
