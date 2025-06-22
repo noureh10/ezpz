b32cfca2642e7e7ffe59187365da94b5abfcdabd
noureh10 <noureh.it@outlook.com>
2025-06-22 23:04:09 +0200

    38 - Updating the Modlist header

--- src/commands/command_header/Modlist.hpp
+++ src/commands/command_header/Modlist.hpp
@@ -2,15 +2,26 @@
  * @ Author: Nour Echaara
  * @ Create Time: 2025-06-02 23:06:23
  * @ Modified by: Nour Echaara
- * @ Modified time: 2025-06-10 14:25:33
+ * @ Modified time: 2025-06-22 15:30:35
  */
 
 #ifndef MODLIST_HPP
 # define MODLIST_HPP
 
 #include "ACommand.hpp"
+#include "../../modlist_manager/WorkshopManager.hpp"
 
 class Modlist : public ACommand {
+    private:
+        // Object accountable for every mod installation and removal
+        WorkshopManager _manager;
+        
+        void showModlist(void);
+        bool installWorkshopCollection(void);
+        bool installSingleMod(unsigned int workshop_id);
+        bool removeMod(void);
+        bool removeWorkshopCollection(void);
+        bool removeAll(void);
     public:
         Modlist(void);
         ~Modlist();
