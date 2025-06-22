d84e0659b5964882aa69c25ec14c1c0e830cc313
noureh10 <noureh.it@outlook.com>
2025-06-22 23:07:52 +0200

    44 - Adding colors macros to be used by commands output

--- src/commands/command_header/ACommand.hpp
+++ src/commands/command_header/ACommand.hpp
@@ -2,7 +2,7 @@
  * @ Author: Nour Echaara
  * @ Create Time: 2025-05-29 19:34:45
  * @ Modified by: Nour Echaara
- * @ Modified time: 2025-06-10 22:07:02
+ * @ Modified time: 2025-06-22 11:53:12
  */
 
 #ifndef ACOMMANDS_HPP
@@ -11,6 +11,21 @@
 #include <iostream>
 #include <map>
 #include <set>
+
+# define RED     "\x1b[31m"
+# define GREEN   "\x1b[32m"
+# define BLUE    "\x1b[34m"
+# define BLACK   "\x1b[30m"
+# define WHITE   "\x1b[37m"
+# define PINK    "\x1b[35m"
+# define YELLOW  "\x1b[33m"
+# define BROWN   "\x1b[33m"
+# define ORANGE  "\x1b[33m"
+# define PURPLE  "\x1b[35m"
+# define GRAY    "\x1b[90m"
+# define LIGHTGRAY "\x1b[37m"
+# define DARKGRAY  "\x1b[90m"
+# define RESET   "\x1b[0m"
 
 class ACommand {
     private:
