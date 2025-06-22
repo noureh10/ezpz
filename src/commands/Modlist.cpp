fbce421bf80ce044fedb458ee6ac7156e763f49b
noureh10 <noureh.it@outlook.com>
2025-06-22 23:04:42 +0200

    39 - Adding the showModlist method

--- src/commands/Modlist.cpp
+++ src/commands/Modlist.cpp
@@ -2,7 +2,7 @@
  * @ Author: Nour Echaara
  * @ Create Time: 2025-06-02 23:08:42
  * @ Modified by: Nour Echaara
- * @ Modified time: 2025-06-10 18:34:47
+ * @ Modified time: 2025-06-22 12:23:24
  */
 
 #include "command_header/Modlist.hpp"
@@ -15,6 +15,22 @@ Modlist::~Modlist(void) {
     
 }
 
+void Modlist::showModlist(void) {
+    const std::unordered_map<unsigned int, std::shared_ptr<WorkshopItem>> items = _manager.getItems();
+    std::shared_ptr<WorkshopItem> it_pointer;
+    unsigned int workshop_id;
+    
+    for (auto it = items.begin(); it != items.end(); it++) {
+        std::cout << BLUE << it->second->getWorkshopName() << RESET << " : "
+        << it->second->getWorkshopModDescription() << " | " 
+        << it->second->getFormattedFilesize() << "\n";
+    }
+}
+
+bool Modlist::installWorkshopCollection(void) {
+    
+}
+
 void Modlist::execute(void) {
-    std::cout << "Placeholder for Modlist command" << std::endl;
+    
 }
