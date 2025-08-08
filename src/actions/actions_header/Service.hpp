67df25dd274725d52de3fe3e98fd22a1ec8bf501
noureh10 <noureh10>
2025-08-08 12:58:50 +0100

    47 Updating Service header

--- src/actions/actions_header/Service.hpp
+++ src/actions/actions_header/Service.hpp
@@ -8,11 +8,12 @@ class Service {
 		std::string service_name;
 		bool is_service_active;
 	public:
+		Service(void);
 		Service(unsigned int s_id, std::string s_name);
 		std::string getServiceName(void);
 		void setServiceName(std::string s_name);
 		unsigned int getServiceId(void);
-		void setServiceId(unsigned int);
+		void setServiceId(unsigned int s_id);
 };
 
 #endif
